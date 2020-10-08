1539931be283fcc22b379fb72023be99c702008e
MinhKhoi2204 <72433767+MinhKhoi2204@users.noreply.github.com>
2020-10-08 17:54:15 +0700

    BTVN số 2

---
+++ BTVN số 2/Untitled5.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int a,b,x,y;
+	printf("nhap so a",a);
+	scanf("%d",&a);
+	printf("nhap so b",b);
+	scanf("%d",&b);
+    if (a>=b){
+    	printf("%d",a/b);
+    	}else{
+    		printf("%d",a*b);
+    		}
+		}
+	
